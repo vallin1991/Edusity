9fb5f41ad3accded4bae67cc5c5c2f01cc30f9ee
Victor Vallin <victorvallinavila@gmail.com>
2024-11-01 21:21:50 -0400

    Created the components About, Title and Programs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 const App = () => {
   return (
     <div>
-    <Navbar/>
-    <Hero/>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+        <Title subTitle='Our PROGRAM' title="What We Offer"/>
+        <Programs/>
+        <About/>
+      </div>
+
     </div>
   )
 }
